b6c0773e549eabf06b397800b4e7db01bfbcb64e
chkummer <chkummer@gmail.com>
2020-01-27 03:43:24 +0100

    moved basic eps functions to lib;added command line arguments to the script

---
+++ Files/lib/EPS_Functions.sh
@@ -0,0 +1,85 @@
+# EPS_Functions.sh: some basic eps funtions for punchcard.sh script
+#
+# For more inforamtion see https://github.com/chkummer/PunchedCard
+#
+#
+# EPS_HEADER
+#
+EPS_HEADER () {
+cat <<=EPS_HEADER_EOF=
+%!PS-Adobe-2.0 EPSF-1.2
+%%Title: PunchedCard (${OUTFILE})
+%%Creator: punchcard.sh (https://github.com/chkummer/PunchedCard)
+%%CreationDate: ${CREATION_DATE}
+%%BoundingBox: 0 0 ${OUT_PAGE_WIDTH} ${OUT_PAGE_HEIGHT}
+%%EndComments
+
+=EPS_HEADER_EOF=
+}
+
+#
+# EPS_INIT_PAGE
+#
+EPS_INIT_PAGE () {
+cat <<=EPS_INIT_PAGE_EOF= 
+%%Page: ${OUT_PAGE_NUM}
+0.5 setlinewidth
+${DOC_BORDER_COLOR} setrgbcolor
+newpath
+0 0 moveto
+${OUT_PAGE_WIDTH} 0 lineto
+${OUT_PAGE_WIDTH} ${OUT_PAGE_HEIGHT} lineto
+0 ${OUT_PAGE_HEIGHT} lineto
+closepath stroke
+=EPS_INIT_PAGE_EOF=
+}
+
+#
+# EPS_CUTTER_FUNCTS
+#
+EPS_CUTTER_FUNCTS () {
+cat <<=EPS_CUTTER_FUNCTS_EOF=
+/Pos 0 def
+/Hole 0 def
+
+/hole
+ { newpath moveto
+   ${CUT_COLOR} setrgbcolor
+   0.25 setlinewidth
+   -1.98 -4.5 rmoveto
+   3.96 0 rlineto
+   0 9 rlineto
+   -3.96 0 rlineto
+   0 -9 rlineto
+   closepath
+   stroke } def
+
+/punchHole
+ { /XPos Pos 6.264 mul 11.808 add def
+   XPos [180 162 144 126 108 90 72 54 36 18 0 198 218] Hole get hole } def
+
+=EPS_CUTTER_FUNCTS_EOF=
+}
+
+#
+# EPS_PRINTER_FUNCTS
+#
+EPS_PRINTER_FUNCTS () {
+cat <<=EPS_PRINTER_FUNCTS_EOF=
+/DotMatrixPattern <00 00 00 00 00 00 00 00> def
+
+/printDotMatrix
+ { newpath
+   /XPos Pos 6.264 mul 9 add def
+   ${DOT_MATRIX_COLOR} setrgbcolor
+   gsave
+   XPos 225 translate
+   6 6 scale
+   8 8 true
+   [8 0 0 8 0 0] { DotMatrixPattern } imagemask
+   grestore } bind def
+
+
+=EPS_PRINTER_FUNCTS_EOF=
+}
+
--- Files/punchcard.sh
+++ Files/punchcard.sh
@@ -5,11 +5,12 @@
 #
 # For more inforamtion see https://github.com/chkummer/PunchedCard
 #
-INPUT_LINE="THIS IS JUST A TEST"
+INPUT_LINE="&-0123456789ABCDEFGHIJKLMNOPQR/STUVWXYZ:#@'=\"¢.<(+|!$*);¬ ,%_>?"
 # default color values using postscript rgb schema
 CUT_COLOR="1 0 0" # red
 CARD_TEXT_COLOR="0 0 0" # black
 DOC_BORDER_COLOR="0 1 0" # green
+DOT_MATRIX_COLOR="0.5 0.5 0.5" # gray
 # default value for the card coding
 CARD_CODE="IBM029"
 # default value for the card type (background printing)
@@ -26,92 +27,91 @@ OUT_PAGE_NUM=1
 OUT_CARD_NUM=1
 # get current date for CREATION_DATE
 CREATION_DATE=`date '+%d-%b-%Y'`
-#
+# 
 # Shell Functions
 #
-# EPS_HEADER
+# USAGE
 #
-EPS_HEADER () {
-cat <<=EPS_HEADER_EOF=
-%!PS-Adobe-2.0 EPSF-1.2
-%%Title: PunchedCard (${OUTFILE})
-%%Creator: punchcard.sh (https://github.com/chkummer/PunchedCard)
-%%CreationDate: ${CREATION_DATE}
-%%BoundingBox: 0 0 ${OUT_PAGE_WIDTH} ${OUT_PAGE_HEIGHT}
-%%EndComments
+USAGE () {
+cat <<=USAGE_EOF=
+usage: $0 [options]
 
-=EPS_HEADER_EOF=
+options are:
+ -i <file>      # input file name
+ -c <code>      # card coding (default: ${CARD_CODE})
+ -C <corners>   # card corners (default: ${CARD_CORNER})
+ -t <type>      # card type (default: ${CARD_TYPE})
+ -h             # this help text
+
+=USAGE_EOF=
+exit
 }
-#
-# EPS_INIT_PAGE
-#
-EPS_INIT_PAGE () {
-cat <<=EPS_INIT_PAGE_EOF= 
-%%Page: ${OUT_PAGE_NUM}
-0.5 setlinewidth
-${DOC_BORDER_COLOR} setrgbcolor
-newpath
-0 0 moveto
-${OUT_PAGE_WIDTH} 0 lineto
-${OUT_PAGE_WIDTH} ${OUT_PAGE_HEIGHT} lineto
-0 ${OUT_PAGE_HEIGHT} lineto
-closepath stroke
-=EPS_INIT_PAGE_EOF=
-}
-#
-# EPS_CUTTER_FUNCTS
-#
-EPS_CUTTER_FUNCTS () {
-cat <<=EPS_CUTTER_FUNCTS_EOF=
-/Pos 0 def
-/Hole 0 def
-
-/hole
- { newpath moveto
-   ${CUT_COLOR} setrgbcolor
-   0.25 setlinewidth
-   -1.98 -4.5 rmoveto
-   3.96 0 rlineto
-   0 9 rlineto
-   -3.96 0 rlineto
-   0 -9 rlineto
-   closepath
-   stroke } def
-
-/punchHole
- { /XPos Pos 6.264 mul 11.808 add def
-   XPos [180 162 144 126 108 90 72 54 36 18 0 198 218] Hole get hole } def
-
-=EPS_CUTTER_FUNCTS_EOF=
-}
-#
-# EPS_PRINTER_FUNCTS
-#
-EPS_PRINTER_FUNCTS () {
-cat <<=EPS_PRINTER_FUNCTS_EOF=
-/DotMatrixPattern <00 00 00 00 00 00 00 00> def
-
-/printDotMatrix
- { newpath
-   /XPos Pos 6.264 mul 9 add def
-   ${CARD_TEXT_COLOR} setrgbcolor
-   gsave
-   XPos 225 translate
-   6 6 scale
-   8 8 true
-   [8 0 0 8 0 0] { DotMatrixPattern } imagemask
-   grestore } bind def
-
-
-=EPS_PRINTER_FUNCTS_EOF=
-}
+if [ -r lib/EPS_Functions.sh ]
+then
+    . lib/EPS_Functions.sh
+else
+    echo "ERROR: can't read lib/EPS_Functions.sh"
+    exit 1
+fi
 #
 # Main
 #
+while getopts i:c:C:t:h option
+do
+    case "${option}" in
+        i) INPUT_FILE=${OPTARG};;
+        c) CARD_CODE=${OPTARG};;
+        C) CARD_CORNER=${OPTARG};;
+        t) CARD_TYPE=${OPTARG};;
+        h) USAGE;;
+    esac
+done
+#
+# checking if all files are acessable
+#
+if [ ! -r ${INPUT_FILE} ]
+then
+    echo "ERROR: can't read input file '${INPUT_FILE}'"
+    exit 1
+fi
 if [ -r lib/${CARD_CODE}_CardCode.sh ]
 then
     . lib/${CARD_CODE}_CardCode.sh
+else
+    echo "ERROR: can't read lib/${CARD_CODE}_CardCode.sh"
+    CARD_CODE_LIST=`ls lib/*_CardCode.sh | sed 's?^.*lib/??g;s?_CardCode.sh??g'`
+    echo "Valid Card Codes are:"
+    for CARD_CODE_ITEM in ${CARD_CODE_LIST}
+    do
+        echo " - ${CARD_CODE_ITEM}"
+    done
+    exit 1
 fi
+if [ ! -r lib/${CARD_CORNER}_CardOutline.ps ]
+then
+    echo "ERROR: can't read lib/${CARD_CORNER}_CardOutline.ps"
+    CARD_CORNER_LIST=`ls lib/*_CardOutline.ps | sed 's?^.*lib/??g;s?_CardOutline.ps??g'`
+    echo "Valid Card Corners are:"
+    for CARD_CORNER_ITEM in ${CARD_CORNER_LIST}
+    do
+        echo " - ${CARD_CORNER_ITEM}"
+    done
+    exit 1
+fi
+if [ ! -r lib/${CARD_TYPE}_CardType.ps ]
+then
+    echo "ERROR: can't read lib/${CARD_TYPE}_CardType.ps"
+    CARD_TYPE_LIST=`ls lib/*_CardType.ps | sed 's?^.*lib/??g;s?_CardType.ps??g'`
+    echo "Valid Card Types are:"
+    for CARD_TYPE_ITEM in ${CARD_TYPE_LIST}
+    do
+        echo " - ${CARD_TYPE_ITEM}"
+    done
+    exit 1
+fi
+#
+# all required files are available; let's go and create some output
+#
 EPS_HEADER > ${OUTFILE}
 echo "%%BeginProcSet" >> ${OUTFILE}
 eval "sed 's/@@CUT_COLOR@@/${CUT_COLOR}/g' lib/${CARD_CORNER}_CardOutline.ps >> ${OUTFILE}"
