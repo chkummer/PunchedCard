1c74794560365321128e0f7821a82cf62aa8e72e
chkummer <chkummer@gmail.com>
2020-03-18 20:48:39 +0100

    added debug information on postscript output

--- Files/punchcard.sh
+++ Files/punchcard.sh
@@ -191,8 +191,9 @@ do
     do
         CURR_CHAR=${INPUT_LINE:${INPUT_POS}:1}
         CONVERT_CHAR
+        echo "% Char: '${CURR_CHAR}'" | tee -a ${OUTFILE} ${PRINTER_OUTFILE} ${CUTTER_OUTFILE} >/dev/null
         echo "/Pos `expr ${INPUT_POS} + 1` def" | tee -a ${OUTFILE} ${PRINTER_OUTFILE} ${CUTTER_OUTFILE} >/dev/null
-        echo "[${HOLES}] { /Hole exch def punchHole} forall" | tee -a ${OUTFILE} ${CUTTER_OUTFILE} >/dev/null
+        echo "[${HOLES}] { /Hole exch def punchHole } forall" | tee -a ${OUTFILE} ${CUTTER_OUTFILE} >/dev/null
         echo "/DotMatrixPattern <${DOT_MATRIX}> def printDotMatrix" | tee -a ${OUTFILE} ${PRINTER_OUTFILE} >/dev/null
         INPUT_POS=`expr ${INPUT_POS} + 1`
         if [ ${INPUT_POS} -eq 80 ]
